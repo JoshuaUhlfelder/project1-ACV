e88477ded7169c4ad248f2e0d45a7587d8e3215f
JoshuaUhlfelder <jfu2001@columbia.edu>
2023-02-28 18:56:26 -0500

    Changed to beit

--- model21.py
+++ model21.py
@@ -282,10 +282,10 @@ imshow(out, title=[class_names[x] for x in classes])
 # Load a pretrained model and reset final fully connected layer for this particular classification problem.
 
 
-image_processor = AutoImageProcessor.from_pretrained('facebook/vit-mae-large')
+image_processor = AutoImageProcessor.from_pretrained("microsoft/beit-base-patch16-224-pt22k")
 
 model = ViTForImageClassification.from_pretrained(
-    'facebook/vit-mae-large',
+    "microsoft/beit-base-patch16-224-pt22k",
     num_labels=num_classes,
     id2label=id2label,
     label2id=label2id,
