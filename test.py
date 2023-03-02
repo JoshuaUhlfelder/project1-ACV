4fc78531e6568fd61d4329b9bc84aec73f98b35f
JoshuaUhlfelder <jfu2001@columbia.edu>
2023-03-02 05:06:20 -0500

    Added acc. vector

--- test.py
+++ test.py
@@ -362,9 +362,12 @@ print('predicting')
 predictions = trainer.predict(image_datasets['test'])
 preds = np.argmax(predictions.predictions, axis=-1)
 trues = predictions.label_ids
-from sklearn.metrics import recall_score, precision_score, accuracy_score
+from sklearn.metrics import recall_score, precision_score, accuracy_score, confusion_matrix
 
 print(accuracy_score(trues, preds))
 print(recall_score(trues, preds, average=None))
 print(precision_score(trues, preds, average=None))
+
+matrix = confusion_matrix(trues, preds)
+print(matrix.diagonal()/matrix.sum(axis=1))
 
